65bd8cbb0691aa3c831def6bda2960c9c6097676
l120191 <licia.belli@gmail.com>
2024-08-29 18:57:10 +0200

    implemented the requirements for peer review of the course final project

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,35 +1,61 @@
-import React from 'react';
+import {React, useState} from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { removeItem, updateQuantity } from './CartSlice';
+import { addItem, removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
 
-const CartItem = ({ onContinueShopping }) => {
+const CartItem = ({ onContinueShopping, setAddedToCart }) => {
   const cart = useSelector(state => state.cart.items);
   const dispatch = useDispatch();
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let totalAmount = 0;
+    cart.map(element => {
+      totalAmount += element.cost * element.quantity;
+    });
+    return totalAmount;
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e)
   };
 
-
-
-  const handleIncrement = (item) => {
+  const handleIncrement = (plant) => { 
+    const newItem = {...plant};
+    newItem.quantity = plant.quantity+1;
+    dispatch(updateQuantity(newItem));   
   };
 
-  const handleDecrement = (item) => {
-   
+  const handleDecrement = (plant) => {
+    if(plant.quantity===1){
+      dispatch(removeItem(plant.name));
+      return;
+    }
+    const newItem = {...plant};
+    newItem.quantity = plant.quantity-1;
+    dispatch(updateQuantity(newItem));   
   };
 
-  const handleRemove = (item) => {
+  const handleRemove = (plant) => { 
+    dispatch(removeItem(plant.name));
+    setAddedToCart(prevState => ({
+      ...prevState,
+      [plant.name]:false,
+  }));
+  };
+
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    let totalCost = 0;
+    cart.find(element=> {
+      if(element.name === item.name)
+        totalCost += element.cost * element.quantity;
+    })
+    return totalCost;
   };
 
   return (
@@ -41,7 +67,7 @@ const CartItem = ({ onContinueShopping }) => {
             <img className="cart-item-image" src={item.image} alt={item.name} />
             <div className="cart-item-details">
               <div className="cart-item-name">{item.name}</div>
-              <div className="cart-item-cost">{item.cost}</div>
+              <div className="cart-item-cost">${item.cost}</div>
               <div className="cart-item-quantity">
                 <button className="cart-item-button cart-item-button-dec" onClick={() => handleDecrement(item)}>-</button>
                 <span className="cart-item-quantity-value">{item.quantity}</span>
@@ -57,7 +83,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
